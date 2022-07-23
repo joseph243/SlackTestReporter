88d94498e0225f8f1a54372b1c122db59e74263a
joseph243 <josephvanderzwart@protonmail.com>
2022-07-23 10:17:01 -0600

    single string

--- index.js
+++ index.js
@@ -25,7 +25,6 @@ try {
   //Slack web hook init:
   const { WebClient } = require('@slack/web-api');
   const web = new WebClient(slackToken);
-  slackBot(`Hello ${nameToGreet}!`);
 
   //file system reader init:
   var fs = require('fs');
@@ -42,7 +41,28 @@ try {
     console.log("Found files: ");
     console.log(data);
 
-    data.forEach(function (fileName) {
+    let FINALSTR = 'Hello ${nameToGreet}!' + "\r\n";
+
+    let promise = new Promise(function(resolve,reject) {
+      FINALSTR += parseData(data);
+      resolve(FINALSTR);
+    });
+
+    promise.then(
+      function(result) { slackBot(FINALSTR) },
+      function(error) { slackBot("Parse failed") }
+    );
+
+
+
+
+  });
+
+  //function for parse every XML:
+  function parseData(inData) {
+    let FULLSTR = "";
+    
+    inData.forEach(function (fileName) {
       // per file actions here, include only TEST files:
       if (fileName.includes('TEST')) {
         //grab file and populate content to JS object:
@@ -57,54 +77,54 @@ try {
         //if (short output stirng for passed test) else (long output string for failed test):
         console.log("calculating test results, deciding if abbreviate output");
         if ( parseInt(content.elements[0].attributes.skipped) == 0 &&
-             parseInt(content.elements[0].attributes.failures) == 0 &&
-             parseInt(content.elements[0].attributes.errors) == 0
-           ) {
-           console.log("building abbreviated string for " + content.elements[0].attributes.name);
-           let SHORTOUTPUT = "" + content.elements[0].attributes.name + " ALL PASSED:";
-           for (let i = 0; i < parseInt(content.elements[0].attributes.tests); i++) {
+              parseInt(content.elements[0].attributes.failures) == 0 &&
+              parseInt(content.elements[0].attributes.errors) == 0
+            ) {
+            console.log("building abbreviated string for " + content.elements[0].attributes.name);
+            let SHORTOUTPUT = "" + content.elements[0].attributes.name + " ALL PASSED:";
+            for (let i = 0; i < parseInt(content.elements[0].attributes.tests); i++) {
             SHORTOUTPUT += " :green_apple:";
-           }
-           SHORTOUTPUT += "\r\n";
-           slackBot(SHORTOUTPUT);
+            }
+            SHORTOUTPUT += "\r\n";
+            FULLSTR += SHORTOUTPUT;
         }
         else {
-           console.log("building long string for failed test: " + content.elements[0].attributes.name);
-           let OUTPUTSTR = "";
-           OUTPUTSTR += content.elements[0].attributes.name;
-           OUTPUTSTR += " HAS ERRORS:";
-           OUTPUTSTR += "\r\n";
+            console.log("building long string for failed test: " + content.elements[0].attributes.name);
+            let OUTPUTSTR = "";
+            OUTPUTSTR += content.elements[0].attributes.name;
+            OUTPUTSTR += " HAS ERRORS:";
+            OUTPUTSTR += "\r\n";
 
-           //begin test case details
-           const testcases = content.elements[0].elements;
-           testcases.forEach(function (item) {
-           if (item.name == "testcase") {
-             var testCaseNameResult = item.attributes.name;
-             testCaseNameResult += getTestCaseResult(item);
-             if (testCaseNameResult.includes('skipped')){
+            //begin test case details
+            const testcases = content.elements[0].elements;
+            testcases.forEach(function (item) {
+            if (item.name == "testcase") {
+              var testCaseNameResult = item.attributes.name;
+              testCaseNameResult += getTestCaseResult(item);
+              if (testCaseNameResult.includes('skipped')){
                 testCaseNameResult = ':pineapple: ' + testCaseNameResult;
               }
-             else if (testCaseNameResult.includes('failure')){
-               var errorMessage = item.elements[0].attributes.message;
-               var shortMessage = errorMessage.split(/\r?\n/)[0];
+              else if (testCaseNameResult.includes('failure')){
+                var errorMessage = item.elements[0].attributes.message;
+                var shortMessage = errorMessage.split(/\r?\n/)[0];
                 testCaseNameResult = ':apple: ' + testCaseNameResult + ' ' 
                   + shortMessage;
               }
-             else {
+              else {
                 testCaseNameResult = ':green_apple: ' + testCaseNameResult;
               }
-             testCaseNameResult = "    " + testCaseNameResult;
-             testCaseNameResult += "\r\n";
-             OUTPUTSTR += testCaseNameResult;
+              testCaseNameResult = "    " + testCaseNameResult;
+              OUTPUTSTR += testCaseNameResult;
             }
     });
-           slackBot(OUTPUTSTR);
+            FULLSTR += OUTPUTSTR + "\r\n";
         }
       }
 
     });
-  });
-
+    return FULLSTR;
+  }
+  
   //function for interpret xml test case:
   function getTestCaseResult(inXML) {
     var outStr = '';
